9cc8591efc01084137b9394dcd924ea0f295dcbf
vanviet2627 <vanviet8367@gmail.com>
2019-04-14 15:49:57 +0700

    Update frontend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {isLogin: true});
 });
 
 router.get('/user', (req, res) => {
