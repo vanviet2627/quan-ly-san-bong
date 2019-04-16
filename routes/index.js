bd758ce953b3e83f2c318bc8886a46bd01b09937
du0ngtrunghieu <dauxanhrauma000@gmail.com>
2019-04-16 21:21:04 +0700

    update home

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const test = require('../models/pitches.Model')
 
+
+// check người dùng đăng nhập
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // console.log(test);
@@ -13,7 +17,7 @@ router.get('/', function(req, res, next) {
   // var t = new test(info)
   // t.save_pitches();
   
-  res.render('index', {isLogin: true});
+  res.render('index', {isLogin: false});
 });
 
 router.get('/user', (req, res) => {
@@ -22,6 +26,32 @@ router.get('/user', (req, res) => {
 
 router.get('/admin', (req, res) => {
   res.render('admin');
+});
+
+router.post('/signin', (req, res) => {
+  res.redirect('/');
+});
+
+router.post('/signup', (req, res) => {
+  res.redirect('/');
+});
+
+router.get('/searchfield', (req, res) => {
+  res.render('schedule', {isLogin: false});
+});
+
+router.get('/payment', (req, res) => {
+  let data = {
+    fieldName: "Sân A",
+    date: "20/4/2019",
+    time: "16:00"
+  }
+  res.render('payment', {isLogin: true, data: data});
+  // res.json({isLogin: true, data: data});
+});
+
+router.get('/payment/qr', (req, res) => {
+  res.render('qr', {isLogin: true});
 })
 
 module.exports = router;
