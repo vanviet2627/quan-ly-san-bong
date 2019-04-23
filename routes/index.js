7140438a290a894cf45c822c61470d2634a7759a
vanviet2627 <vanviet8367@gmail.com>
2019-04-24 00:09:29 +0700

    Fix

--- routes/index.js
+++ routes/index.js
@@ -1,83 +1,68 @@
 var express = require('express');
 var router = express.Router();
-const SanBong = require('../models/pitches.Model').Sanbong
-const lichDatSan = require('../models/schedule.Model')
+const Pitch = require('../models/pitches.Model').Sanbong
+const Schedule = require('../models/schedule.Model')
 
-// check người dùng đăng nhập
-
-/* GET home page. */
+/* Get Home Page. */
 router.get('/', (req, res, next) => {
-
   res.render('index', {
     isLogin: false,
     info: {},
     data : null
   });
 });
-// api login ảo lòi :))
 
-router.post('/checklogin',async (req, res) => {
-    let myData = req.body
-    console.log(myData);
-    
-   
-    let dataSanBong = await SanBong.find({})
-    
-    
-    if (myData.username == 'admin' || myData.username == 'user@gmail.com') {
-      if (myData.password == 'admin' || myData.password == '123') {
-        let info = {
-          acp: 1,
-        }
-        res.json(info)
-      } else {
-        let info = {
-          mess: "tài khoản, mật khẩu không đúng . Vui lòng Kiểm Tra lại",
-          acp: 0
-        }
-        res.json(info)
-      }
+// ==========================================
+//                   PAYMENT
+// ==========================================
 
-    } else {
-      let info = {
-        mess: "tài khoản, mật khẩu không đúng . Vui lòng Kiểm Tra lại",
-        acp: 0
-      }
-      res.json(info)
-    }
-  })
-  .get('/checklogin',async (req, res) => {
+// QR API link
+router.get('/payment/ewallet/:type', (req, res) => {
+  let type = req.params.type;
+  let eWallet = type == "momo" ? "link momo payment" : "link zalopay payment";
+  let data = {
+    paymentLink: `<img src='http://chart.apis.google.com/chart?cht=qr&chl=${eWallet}&chs=300' alt='qr' />`
+  }
+  res.status(200).json({data: data});
+});
 
-      res.redirect('/')    
-  })
+router.post('/payment', async(req, res, next) => {
+  let info = req.body;
+  let bill = {
+    sanbong: info.loaisan,
+    ngayThue: info.ngay,
+    dattu: info.gio,
+    thoiluongThue: info.thoiluong,
+    email: "user@gmail.com",
+  }
+  var t = new Schedule(bill)
+  let dataBeforSave = await t.add_schedule()
+  res.render('payment', {isLogin: true, data: dataBeforSave});
+})
+
+router.post('/payment/type-ewallet', (req, res) => {
+  let typeOfEWallet = req.body.exampleRadios;
+  // Get link API E-Wallet
+  res.render('qr', {isLogin: true, typeEwallet: typeOfEWallet});
+});
+
+// ==========================================
+//                   User
+// ==========================================
+
 // api đặt sân 
-router.get('/s/',async(req,res)=>{
-  let dataSanBong = await SanBong.find({})
+router.get('/s/', async(req, res)=>{
+  let dataSanBong = await Pitch.find({})
   res.render('index', {
     isLogin: true,
     info: {},
     data: dataSanBong
   })
 })
-router.post('/payment', async(req, res, next) => {
-  let myData = req.body
-  console.log(myData);
-  
-  let info = {
-    sanbong : myData.loaisan,
-    ngayThue:myData.ngay,
-    dattu:myData.gio,
-    thoiluongThue:myData.thoiluong,
-    email: "user@gmail.com",
-  }
-  var t = new lichDatSan(info)
-  let dataBeforSave = await t.add_schedule()
 
-  res.render('payment', {isLogin: true, data: dataBeforSave});
-})
 // 
 router.get('/user',async (req, res) => {
-  let dataSanBong = await SanBong.find({})
+  let dataSanBong = await Pitch.find({})
   res.render('User', {
     isLogin: true,
     info: {},
@@ -103,45 +88,6 @@ router.get('/searchfield', (req, res) => {
   });
 });
 
-// ==========================================
-//                   PAYMENT
-// ==========================================
-
-// QR API link
-router.get('/payment/ewallet/', (req, res) => {
-  let eWallet = "Đấng óc chó Ahihi";
-  let data = {
-    paymentLink: `<img src='http://chart.apis.google.com/chart?cht=qr&chl=${eWallet}&chs=300' alt='qr' />`
-  }
-  res.status(200).json({data: data});
-});
-
-router.get('/payment', (req, res) => {
-  let data = {
-    fieldName: "Sân A",
-    date: "20/4/2019",
-    time: "16:00",
-    email: "asd@gmail.com",
-     sdt: "0968222222"
-  }
-  res.render('payment', {isLogin: true, data: data});
-});
-
-router.post('/payment_viet', (req, res) => {
-  let info = {
-    phoneNumber: req.body.phoneNumber,
-    typeOfEWallet: req.body.exampleRadios
-  }
-  console.log(info);
-  // Get link API E-Wallet
-
-  res.redirect('/qr');
-});
-
-router.get('/qr', (req, res) => {
-  res.render('qr', {isLogin: true});
-});
-
 router.get('/Admin', (req, res) => {
   res.render('Admin', {
     isLogin: true
@@ -159,5 +105,34 @@ router.get('/account', (req, res) => {
     isLogin: true
   });
 });
+
+router.post('/checklogin',async (req, res) => {
+  let myData = req.body
+  console.log(myData);
+  let dataSanBong = await Pitch.find({})
+  if (myData.username == 'admin' || myData.username == 'user@gmail.com') {
+    if (myData.password == 'admin' || myData.password == '123') {
+      let info = {
+        acp: 1,
+      }
+      res.json(info)
+    } else {
+      let info = {
+        mess: "Tài khoản hoặc mật khẩu không đúng.",
+        acp: 0
+      }
+      res.json(info)
+    }
+  } else {
+    let info = {
+      mess: "Tài khoản hoặc mật khẩu không đúng.",
+      acp: 0
+    }
+    res.json(info)
+  }
+})
+.get('/checklogin', async (req, res) => {
+    res.redirect('/')    
+})
 
 module.exports = router;
